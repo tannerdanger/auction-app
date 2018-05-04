e1af5f48c69d85ac8536be7f311bb5d65011f7e2
shuwen123 <shuwenroy@yahoo.com>
2018-05-04 14:25:59 -0700

    updated auctiondata
    Updated auction and auctionItem class

--- src/auctiondata/Auction.java
+++ src/auctiondata/Auction.java
@@ -1,27 +1,82 @@
+/*
+ * TCSS 360 deliverable 2
+ * Group 4
+ */
 package auctiondata;
 
 import java.time.LocalDate;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import auctiondata.AuctionItem;
 
 /**
- * An object representing an auction
+ * An object representing an auction included in an calendar.
+ * @author Wen Shu
+ * @version May 4th 2018
  */
 public class Auction {
 
-	LocalDate auctionDate;
+	private LocalDate auctionDate;
+	
+	private Date auctionTime;
+	
+	private final String organizationName;
+	
+	private final String organizationID;
 
-
-	public Auction(){
-
+	private Map<String, AuctionItem> inventorySheet = new HashMap<String, AuctionItem>();
+	
+	/**
+	 * A private constructor to prevent the default constructor to create an null auction.
+	 * User has to provide all item info to create the auction class object.
+	 */
+	private Auction(){
+		organizationName = null;
+		organizationID = null;
 	}
-
+	
+	public Auction(final String theOrganizationName,
+				   final String theOranizationID,
+				   final LocalDate theAuctionDate,
+				   final Date theAuctionTime,
+				   Map<String, AuctionItem> theInventorySheet) {
+		organizationName = theOrganizationName;
+		organizationID = theOranizationID;
+		auctionDate = theAuctionDate;
+		auctionTime = theAuctionTime;
+		inventorySheet = theInventorySheet;
+	}
+	
+	public void addInventoryItem(final AuctionItem theItem) {
+		inventorySheet.put(theItem.getUniqueID(), theItem);
+	}
+	
+	public void removeInventoryItem(final String theUniqueID) {
+		inventorySheet.remove(theUniqueID);
+	}
+	
+	public void setAuctionDate(LocalDate auctionDate) {
+		this.auctionDate = auctionDate;
+	}
+	
+	@SuppressWarnings("deprecation")
+	public void setAuctionTime(int theHours) {
+		auctionTime.setHours(theHours);
+	}
 
 	public LocalDate getAuctionDate() {
 		return auctionDate;
 	}
 
-	public void setAuctionDate(LocalDate auctionDate) {
-		this.auctionDate = auctionDate;
+	@SuppressWarnings("deprecation")
+	public int getAuctionTime() {
+		return auctionTime.getHours();
+	}
+	
+	public Map<String, AuctionItem> getInventorySheet(){
+		return inventorySheet;
 	}
 
 }
--- src/auctiondata/AuctionItem.java
+++ src/auctiondata/AuctionItem.java
@@ -1,7 +1,69 @@
+/*
+ * TCSS 360 deliverable 2
+ * Group 4
+ */
 package auctiondata;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
 
 /**
  * An object representing an item included in an auction
+ * @author Wen Shu
+ * @version May 4th 2018
  */
 public class AuctionItem {
+	private BigDecimal minPrice;
+	
+	private String name;
+	
+	private final String uniqueID;
+	
+	private Map<String, BigDecimal> sealedBids = new HashMap<String, BigDecimal>();
+	
+	/**
+	 * A private constructor to prevent the default constructor to create an null auction item.
+	 * User has to provide all item info to create the auction item class object.
+	 */
+	private AuctionItem() {
+		uniqueID = null;
+		minPrice = null;
+		name = null;
+	}
+	
+	public AuctionItem(final double theMinPrice, String theName, String theUniqueID) {
+		minPrice = new BigDecimal(theMinPrice);
+		name = theName;
+		uniqueID = theUniqueID;
+	}
+	
+	public void setName(String theName) {
+		name = theName;
+	}
+	
+	public void setMinPrice(double thePrice) {
+		minPrice = new BigDecimal(thePrice);
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public double getMinPrice() {
+		return minPrice.doubleValue();
+	}
+	
+	public String getUniqueID() {
+		return uniqueID;
+	}
+	
+	public void addSealedBids(String theBidderID, BigDecimal theSealedBid) {
+		sealedBids.put(theBidderID, theSealedBid);
+	}
+	
+	public Map<String, BigDecimal> getSealedBids(){
+		return sealedBids;
+	}
 }
