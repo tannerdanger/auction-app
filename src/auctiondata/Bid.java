57ac463b3efb45d96593e52bdcd6671d837f5fe3
Charlie Grumer <cgrumer@uw.edu>
2018-05-05 21:31:13 -0700

    Updated Bidder and Bid.
    Bidder: changed placeBid params, changed constructor, added bids list getter
    Bid: changed fields, added constructor, added boolean to check if bid place was successful

--- src/auctiondata/Bid.java
+++ src/auctiondata/Bid.java
@@ -1,7 +1,25 @@
 package auctiondata;
 
+import java.math.BigDecimal;
+
 /**
  * An object representing a user's bid
  */
 public class Bid {
+	
+	final Auction whichAuction;
+	final AuctionItem item;
+	final BigDecimal sealedBidAmount;
+	final Boolean placedBid;
+	
+	public Bid(final Auction theAuction, final BigDecimal theBidAmount, final AuctionItem theItem) {
+		whichAuction = theAuction;
+		sealedBidAmount = theBidAmount;
+		item = theItem;
+		placedBid = false;
+	}
+	
+	public boolean isBidPlaced() {
+		return placedBid;
+	}
 }
--- src/users/Bidder.java
+++ src/users/Bidder.java
@@ -1,19 +1,45 @@
 package users;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import auctiondata.Auction;
+import auctiondata.AuctionItem;
+import auctiondata.Bid;
+
 /**
+ * @author Tanner Brown, Charles Grumer
+ * @version 05/05/18
  * Bidder class, saves relevent information to a bidder
  */
 public class Bidder extends User {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2136532391266767810L;
+	
 	//TODO: Define values associated with a bidder. i.e. an array of auctions that bids have been placed in?
 	//And/or an array of auction items that have been bid on?
+	private final List<Bid> myBids;
 
 	public Bidder(String theFirst, String theLast, String theEmail) {
 		super(theFirst, theLast, theEmail); //pass basic ID values to User superclass
+		myBids = new ArrayList<>();
 	}
 
-
-	public static void placeBid() {
+	public void placeBid(final Auction theAuction, final BigDecimal theBidAmount, final AuctionItem theItem) {
 		//Todo: Prompt user for info for a bid to place, then try to create a new Bid object and add it to the bidder's bid array
+		final Bid b = new Bid(theAuction, theBidAmount, theItem);
+		if (b.isBidPlaced()) {
+			myBids.add(b);
+		} else {
+			System.out.println("Bid failed to be placed");
+		}
+	}
+	
+	public List<Bid> getBids() {
+		return new ArrayList<Bid>(myBids);
 	}
 }
