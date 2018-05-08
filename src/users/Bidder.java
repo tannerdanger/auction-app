aadc568ab1d5a86771170c2728e469cfa3c61359
Guri <guri@uw.edu>
2018-05-07 23:17:55 -0700

    Updated with bidcoutner

--- src/users/Bidder.java
+++ src/users/Bidder.java
@@ -2,9 +2,6 @@ package users;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.chrono.ChronoLocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -29,33 +26,29 @@ public class Bidder extends User implements Serializable {
     //And/or an array of auction items that have been bid on?
     private final List<Bid> myBids;
     private final String myID;
+    private int myBidCounter;
 
     public Bidder(String theFirst, String theLast, String theEmail) {
         super(theFirst, theLast, theEmail); //pass basic ID values to User superclass
         myBids = new ArrayList<>();
         myID = theEmail;
+        myBidCounter = 0;
     } 
 
 
     public List<Bid> getBids() {
         return new ArrayList<Bid>(myBids);
     }
-
-    public static boolean isDateValid(LocalDate theAuctionDate) {
-    	boolean result = false;
-       	if(LocalDate.now().isBefore(theAuctionDate)) {
-    		result = true;
-    	}
-    	return result;
-    }
     
+
     public boolean placeBid(final Auction theAuction, final BigDecimal theBidAmount, final AuctionItem theItem) {
         //Todo: Prompt user for info for a bid to place, then try to create a new Bid object and add it to the bidder's bid array
         final Bid b = new Bid(theAuction, theBidAmount, theItem);
         boolean result = false;
-        if (b.isBidPlaced()) {
+        if (b.isBidPlaced() && (myBidCounter < 4)) {
             myBids.add(b);
             theItem.addSealedBids(myID, theBidAmount);
+            myBidCounter++;
             result = true;
         }
         return result;
