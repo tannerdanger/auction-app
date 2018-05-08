3b11b2fbf0abbb463aab66b15044b8eeb868b3a0
Charlie Grumer <cgrumer@uw.edu>
2018-05-07 22:10:45 -0700

    auctiondate junit tests fixed

---
+++ src/tests/AuctionDate.java
@@ -0,0 +1,65 @@
+package tests;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import auctiondata.Auction;
+import auctiondata.AuctionItem;
+import users.Bidder;
+
+public class AuctionDate {
+
+	    private static final double MIN_PRICE = 99;
+	    private static final int AUCTIONID = 12345;
+	    Bidder bidder;
+	    Auction auction;
+	    AuctionItem item;
+	    /**
+	     * @throws java.lang.Exception
+	     */
+	    @Before
+	    public void setUp() throws Exception {
+	        bidder = new Bidder("wen", "shu", "bidder_wen@bidder.com");
+	        item = new AuctionItem(MIN_PRICE, "answers for TCSS 360 final");
+	        auction = new Auction("UWTacoma", AUCTIONID, LocalDateTime.of(2018, 05, 25, 12, 00), null);
+	        auction.addInventoryItem(item);
+	    }
+
+	    @Test
+	    public void placeBid_bidIsGreaterThanMinPrice_True() {
+	        assertTrue(bidder.placeBid(auction, new BigDecimal(MIN_PRICE + 1), item));
+	    }
+
+	    @Test
+	    public void placeBid_bidIsEqualtoMinPrice_True() {
+	        assertTrue(bidder.placeBid(auction, new BigDecimal(MIN_PRICE), item));
+	    }
+
+	    @Test
+	    public void placeBid_bidIsLessThanMinPrice_False() {
+	        assertFalse(bidder.placeBid(auction, new BigDecimal(MIN_PRICE - 1), item));
+	    }
+	    
+	    @Test
+	    public void placeBid_DateBeforeAuction_true() {
+	    	assertTrue(bidder.isDateValid(LocalDate.now().plusDays(1)));
+	    }
+	    @Test
+	    public void placeBid_DateOfAuction_false() {
+	    	assertFalse(bidder.isDateValid(LocalDate.now()));
+
+	    }
+	    @Test
+	    public void placeBid_DateAfterAuction_false() {
+	    	assertFalse(bidder.isDateValid(LocalDate.now().minusDays(1)));
+
+	    }
+	}
+
