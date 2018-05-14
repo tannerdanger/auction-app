d15cc54e8c86ca07d077d5bc642dd6642ea27ca2
Charlie Grumer <cgrumer@uw.edu>
2018-05-13 20:13:55 -0700

    Updated bidder gui

--- src/GUI/BidderGUI.java
+++ src/GUI/BidderGUI.java
@@ -7,11 +7,13 @@ import java.math.BigDecimal;
 import java.text.NumberFormat;
 import java.util.Map;
 
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JFormattedTextField;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 import javax.swing.JSplitPane;
 import javax.swing.JTabbedPane;
 import javax.swing.event.ListSelectionEvent;
@@ -19,24 +21,34 @@ import javax.swing.event.ListSelectionListener;
 
 import auctiondata.Auction;
 import auctiondata.AuctionItem;
+import auctiondata.Bid;
 import storage.AuctionCalendar;
 import users.Bidder;
 
 public final class BidderGUI extends JPanel {
-
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 3761509683757395546L;
 	private final AuctionCalendar myCalendar;
 	private final Bidder myBidder;
-	private final JPanel myBidBar;
-	private JList<String> myCurrentAuctionItemsPanel;
+	private final BidBar myBidBar;
+	private Auction myCurrentAuction;
+	private AuctionItem myCurrentItem;
+	private JPanel myViewAllItemsInAuction;
+	private JPanel myViewAllItemsinAuctionWithMyBids;
+	private JPanel myCurrentAuctionItemsPanel;
+	private JPanel myRadioPanel;
 
 	public BidderGUI(final Bidder theBidder, final AuctionCalendar theCalendar) {	
 		myCalendar = theCalendar;
 		myBidder = theBidder;
-		myCurrentAuctionItemsPanel = new JList<String>();
+		myCurrentAuction = null;
+		myCurrentItem = null;
+		myRadioPanel = createRadioPanel();
+		myCurrentAuctionItemsPanel = new JPanel();
+		myViewAllItemsInAuction = new JPanel();
+		myViewAllItemsinAuctionWithMyBids = new JPanel();
 		final JTabbedPane tabs = setupTabs();
 		myBidBar = new BidBar();
 		this.add(tabs, BorderLayout.CENTER);
@@ -44,20 +56,56 @@ public final class BidderGUI extends JPanel {
 		this.setVisible(true);
 	}	
 	
+	private JPanel createRadioPanel() {
+		final JPanel radioPanel = new JPanel();
+		final JRadioButton allItemsAvailableInAuction = new JRadioButton();
+		final JRadioButton allItemsInAuctionWithMybids = new JRadioButton();
+		final ButtonGroup radioButtonGroup = new ButtonGroup();
+		radioButtonGroup.add(allItemsInAuctionWithMybids);
+		radioButtonGroup.add(allItemsAvailableInAuction);
+		radioPanel.add(allItemsInAuctionWithMybids);
+		radioPanel.add(allItemsAvailableInAuction);
+		
+		return radioPanel;
+	}
+	
 	private JTabbedPane setupTabs() {
 		final JTabbedPane tabs = new JTabbedPane();
 		
 		final JSplitPane viewAllBiddableAuctions = createViewBiddableAuctionsPanel();
-		final JPanel viewAllItemsWithMyBids = new JPanel();
+		final JPanel viewAllItemsWithMyBids = createViewAllItemsWithMyBids();
 		tabs.addTab("View auctions you can bid in", viewAllBiddableAuctions);
 		tabs.addTab("View your bids", viewAllItemsWithMyBids);
 		
-		final JPanel viewAllItemsInAuction = new JPanel();
-		final JPanel viewAllItemsInAuctionWithMyBids = new JPanel();
-		tabs.add(viewAllItemsInAuction);
-		tabs.add(viewAllItemsInAuctionWithMyBids);
-		
 		return tabs;
+	}
+	
+	private JPanel createViewAllItemsWithMyBids() {
+		final JPanel panel = new JPanel();
+		final String[] bids = new String[myBidder.getBids().size()];
+		int i = 0;
+		for(final Bid b : myBidder.getBids()) {
+			bids[i] = getBidString(b);
+			i++;
+		}
+		final JList<String> bidsList = new JList<>(bids);
+		panel.add(bidsList);
+		return panel;
+	}
+	
+	private String getBidString(final Bid theBid) {
+		final StringBuilder sb = new StringBuilder();
+		sb.append(theBid.getItem().getName() + " from " + theBid.getAuction().getOrgName() 
+				  + " | Bid: $" + theBid.getBidAmount());
+		return sb.toString();
+
+	}
+	
+	private String getAuctionItemString(final AuctionItem theItem) {
+		final StringBuilder sb = new StringBuilder();
+		sb.append("ID: " + theItem.getUniqueID() + " | " + theItem.getName() 
+		  + " | Min Price: $" + theItem.getMinPrice());
+		return sb.toString();
 	}
 
 	private JSplitPane createViewBiddableAuctionsPanel() {		
@@ -66,19 +114,26 @@ public final class BidderGUI extends JPanel {
 		pane.setRightComponent(myCurrentAuctionItemsPanel);
 		return pane;
 	}
-	
-	private void updateAuctionItemsList(final int theAuctionIndex) {
-		final Auction auction = myCalendar.getActiveAuctions().get(theAuctionIndex);
-		final Map<Integer, AuctionItem> items = auction.getInventorySheet();
+
+	private void updateAuctionItemsList() {
+		final Map<Integer, AuctionItem> items = myCurrentAuction.getInventorySheet();
 		final String[] itemStrings = new String[items.size()];
 		for(final Integer i : items.keySet()) {
-			final StringBuilder sb = new StringBuilder();
 			final AuctionItem item = items.get(i);
-			sb.append("ID: " + item.getUniqueID() + " | " + item.getName() 
-					  + " | Min Price: $" + item.getMinPrice());
-			itemStrings[i] = sb.toString();
+			itemStrings[i] = getAuctionItemString(item);
 		}
-		myCurrentAuctionItemsPanel = new JList<String>(itemStrings);
+		final JList<String> itemList = new JList<>(itemStrings);
+		myCurrentAuctionItemsPanel.setLayout(new BorderLayout());
+		myCurrentAuctionItemsPanel.add(itemList, BorderLayout.CENTER);
+		itemList.addListSelectionListener(new ListSelectionListener() {
+			@Override
+			public void valueChanged(final ListSelectionEvent theEvent) {
+				final int index = theEvent.getLastIndex();
+				myCurrentItem = myCurrentAuction.getInventorySheet().get(index);
+				myBidBar.setBidItem();
+			}
+		});		
+		myCurrentAuctionItemsPanel.add(myRadioPanel, BorderLayout.SOUTH);
 	}
 	
 	private JList<String> createAuctionsList() {
@@ -97,7 +152,8 @@ public final class BidderGUI extends JPanel {
 
 			@Override
 			public void valueChanged(final ListSelectionEvent theArg) {
-				updateAuctionItemsList(theArg.getLastIndex());
+				myCurrentAuction = myCalendar.getAuction(theArg.getLastIndex());
+				updateAuctionItemsList();
 			}
 		});
 		
@@ -105,6 +161,10 @@ public final class BidderGUI extends JPanel {
 	}
 
 	public class BidBar extends JPanel {
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1450666832316412151L;
 		private JLabel myLabel;
 		private JFormattedTextField myBidAmount;
 		private JButton myBidButton;
@@ -120,10 +180,10 @@ public final class BidderGUI extends JPanel {
 			});
 		}
 		
-		public void setBidItem(final Auction theAuction, final AuctionItem theItem) {
-			myLabel.setText(theItem.getName() + " $");
+		public void setBidItem() {
+			myLabel.setText(myCurrentItem.getName() + " $");
 			final BigDecimal bid = new BigDecimal(((Number)myBidAmount.getValue()).doubleValue());
-			setBidBarStatus(myBidder.isBidPlaceable(theAuction, theItem, bid));
+			setBidBarStatus(myBidder.isBidPlaceable(myCurrentAuction, myCurrentItem, bid));
 		}
 		
 		private void setBidBarStatus(final boolean theStatus) {
