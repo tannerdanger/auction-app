ab23440777825340123a1aee90933f4ea2fab9ff
shuwen123 <shuwenroy@yahoo.com>
2018-05-21 15:57:56 -0700

    modified auction class by adding hasBid() method to check if the auction has any bids.

--- src/auctiondata/Auction.java
+++ src/auctiondata/Auction.java
@@ -95,6 +95,14 @@ public class Auction implements Serializable {
 	public void setAuctionDate(LocalDate auctionDate) {
 		this.auctionDate = auctionDate;
 	}
+	
+	public boolean hasBid() {
+		boolean result = false;
+		for(Map.Entry<Integer, AuctionItem> entry : inventorySheet.entrySet()) {
+			result = !(entry.getValue().getSealedBids().isEmpty());
+		}
+		return result;
+	}
 
 
 	public void setAuctionTime(String theHours) {
